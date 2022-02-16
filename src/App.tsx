57ea68ea52da769c54525fa875e935ee9f30c6a7
iamidlek <iamidlek@naver.com>
2022-02-16 15:23:47 +0900

    conf: webpack, loader setting and test

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const App = () => {
+  return (
+    <div>
+      <img src="./images/stamp.png" alt="test" />
+    </div>
+  );
+};
+
+export default App;
---
+++ webpack.config.js
@@ -0,0 +1,40 @@
+const HtmlWebpackPlugin = require("html-webpack-plugin");
+const path = require("path");
+
+module.exports = {
+  entry: "./src/index.tsx",
+  resolve: {
+    extensions: [".js", ".jsx", ".ts", ".tsx", ".scss"],
+  },
+  module: {
+    rules: [
+      {
+        test: /\.s?css$/,
+        use: ["style-loader", "css-loader", "postcss-loader", "sass-loader"],
+      },
+      {
+        test: /\.tsx?$/,
+        exclude: /node_modules/,
+        use: ["babel-loader", "ts-loader"],
+      },
+      {
+        test: /\.(png|jpe?g|gif|webp)$/,
+        use: "file-loader",
+      },
+    ],
+  },
+  output: {
+    path: path.join(__dirname, "/dist"),
+    filename: "bundle.js",
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./public/index.html",
+    }),
+  ],
+  devServer: {
+    host: "localhost",
+    port: 3050,
+    hot: true,
+  },
+};
