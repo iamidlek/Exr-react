2f51d757c773a9c77728d504172a0e1b842ff2a4
iamidlek <iamidlek@naver.com>
2022-03-08 10:36:41 +0900

    conf: create googlemap project

---
+++ googlemap/environment.d.ts
@@ -0,0 +1,11 @@
+// declare global {
+namespace NodeJS {
+  interface ProcessEnv {
+    NEXT_PUBLIC_GOOGLE_MAPS_API_KEY: string;
+    // NODE_ENV: 'development' | 'production';
+    // PORT?: string;
+
+    // [key: string]: string | undefined;
+  }
+}
+// }
