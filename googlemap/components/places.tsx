2ed8bafb55771bbf24f65d0c46c3fc0cdfebcc21
iamidlek <iamidlek@naver.com>
2022-03-08 10:48:32 +0900

    feat: search and get latlng

---
+++ googlemap/components/places.tsx
@@ -0,0 +1,54 @@
+import usePlacesAutocomplete, {
+  getGeocode,
+  getLatLng,
+} from 'use-places-autocomplete';
+import {
+  Combobox,
+  ComboboxInput,
+  ComboboxPopover,
+  ComboboxList,
+  ComboboxOption,
+} from '@reach/combobox';
+import '@reach/combobox/styles.css';
+
+type PlacesProps = {
+  setOffice: (position: google.maps.LatLngLiteral) => void;
+};
+
+export default function Places({ setOffice }: PlacesProps) {
+  const {
+    ready,
+    value,
+    setValue,
+    suggestions: { status, data },
+    clearSuggestions,
+  } = usePlacesAutocomplete();
+
+  const handleSelect = async (val: string) => {
+    setValue(val, false); // 값만 갱신 fetch 안함 (선택 했기 때문에 추가 제안 불필요)
+    clearSuggestions(); // 추천 검색 비우기
+
+    const results = await getGeocode({ address: val });
+    const { lat, lng } = await getLatLng(results[0]);
+    setOffice({ lat, lng });
+  };
+
+  return (
+    <Combobox onSelect={handleSelect}>
+      <ComboboxInput
+        value={value}
+        onChange={(e) => setValue(e.target.value)}
+        className="combobox-input"
+        placeholder="Search office address"
+      />
+      <ComboboxPopover>
+        <ComboboxList>
+          {'OK' === status &&
+            data.map(({ place_id, description }) => (
+              <ComboboxOption key={place_id} value={description} />
+            ))}
+        </ComboboxList>
+      </ComboboxPopover>
+    </Combobox>
+  );
+}
